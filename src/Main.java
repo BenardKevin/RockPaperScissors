cc7c02f138fa356fad1146f75cbf5073ad3fa0db
Kbenard <kevin.benard00@gmail.com>
2024-10-11 16:11:28 +0200

    Merge game mecanic and country selection - Update round selection

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Map<String, String[]> winConditions = new HashMap<>();
-        winConditions.put("Pierre", new String[]{"Ciseaux"});
-        winConditions.put("Papier", new String[]{"Pierre"});
-        winConditions.put("Ciseaux", new String[]{"Papier"});
-        CountrySelection countryselection = new CountrySelection();
+
+        CountrySelection countrySelection = new CountrySelection(winConditions);
         RockPaperScissors game = new RockPaperScissors(winConditions);
 
-        CountrySelection.countryselection();
-        int nbPlay = 10;
+        countrySelection.countryselection();
+
+        Scanner round = new Scanner(System.in);
+        System.out.println("How many round to play ?: ");
+        int nbPlay = round.nextInt();
+
         game.play(nbPlay);
+
+        round.close();
     }
 }
